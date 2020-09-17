cee346b31f27f77698aa5d423ce3244392be8261
kittencoder <niklas.becker.dev@gmail.com>
2020-09-17 16:39:47 +0200

    refactor code, add feature to evade mouse

--- drag_window.fish
+++ drag_window.fish
@@ -12,6 +12,8 @@ function drag_window --description "drag the clicked window to all visited works
     set -l margin 50
     set -l marginh false
     set -l targetclass floating
+    set -l evade false
+    set -l evadeEnabled false
     for arg in $argv
         if [ "$setw" = true ]
             set setw $arg
@@ -41,6 +43,11 @@ function drag_window --description "drag the clicked window to all visited works
             set targetclass $arg
             continue
         end
+        if [ "$evade" = true ]
+            # set evade $arg
+            set evadeEnabled true
+            continue
+        end
 
         switch $arg
             case current
@@ -59,6 +66,9 @@ function drag_window --description "drag the clicked window to all visited works
                 set marginh true
             case targetclass
                 set targetclass true
+            case evade
+                set evade true
+                set evadeEnabled true
             case tr
                 set position tr
             case tl
@@ -94,6 +104,8 @@ function drag_window --description "drag the clicked window to all visited works
                 echo ""
                 echo -e "[\e[33mtargetclass \e[35mword\e[0m] : defaults to \e[94m$targetclass\e[0m, the window-class to be set as floating-active (useful for making it transparent, etc.)"
                 echo ""
+                echo -e "           [\e[33mevade\e[0m] : defaults to \e[94m$evade\e[0m, when activated the window will evade the mouse quadrant of the screen, to click the window, focus it (via keyboard-focus-floating?)"
+                echo ""
                 echo -e "     [\e[33mcheckSystem\e[0m] : checks the system for executables and provides pipeable installation instructions"
                 echo ""
                 return
@@ -126,14 +138,82 @@ function drag_window --description "drag the clicked window to all visited works
         i3-msg "[id=\"$wid\"] floating toggle" >/dev/null
     end
     # set floating-class on window
-    echo -e "[\e[91m BXDSWFC \e[0m]"
     echo -e "Setting class on window \e[93m$wname\e[0m to \e[93m$targetclass\e[0m"
     xprop -id $wid -f WM_CLASS 8s -set WM_CLASS $targetclass
-    echo -e "[\e[91m AXDSWFC \e[0m]"
 
     # Announce script starting
-    echo -e "Dragging along window \e[95m$wname\e[0m [\e[36m$wid\e[0m]"
+    echo -e "Dragging along window \e[95m$wname\e[0m [\e[36m$wid\e[0m] TO $position"
+    DRAG_WINDOW_setCorner $wid $setw $seth $margin $marginh $wfac $position
 
+    ###
+    ### Main window dragging script
+    ###
+    ##  updates whenever a window is changed
+    ##  -> to use less resources, and detect workspace-changes upon that
+    ##
+    set pws UNSET
+    set lmouseCorner NONE
+    set cpos $position
+    while true
+        # main function loop
+        # monitor for switching workspaces
+        set cws (i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
+        if [ "$cws" != "$pws" ]
+            # move window if workspace changed and update last move target
+            i3-msg '[id="'$wid'"] move to workspace '$cws >/dev/null
+            if [ "$pws" != UNSET ]
+                echo -e "Moving \e[95m$wname\e[0m from WS \e[33m$pws\e[0m to \e[92m$cws\e[0m"
+            end
+            set pws $cws
+        else if $evadeEnabled
+            set -l windowFocused (i3-msg -t get_tree | jq '.. | select(.window==33554437)? | .focused')
+            if ! $windowFocused
+                set -l c (DRAG_WINDOW_getCorner)
+                if [ "$lmouseCorner" != "$c" ]
+                    set lmouseCorner $c
+                    if [ "$c" = "$position" ]
+                        switch $position
+                            case tl
+                                DRAG_WINDOW_setCorner $wid $setw $seth $margin $marginh $wfac bl
+                                set cpos bl
+                            case tr
+                                DRAG_WINDOW_setCorner $wid $setw $seth $margin $marginh $wfac br
+                                set cpos br
+                            case bl
+                                DRAG_WINDOW_setCorner $wid $setw $seth $margin $marginh $wfac tl
+                                set cpos tl
+                            case br
+                                DRAG_WINDOW_setCorner $wid $setw $seth $margin $marginh $wfac tr
+                                set cpos tr
+                        end
+                    else
+                        # if in main quadrant, if not move back
+                        DRAG_WINDOW_setCorner $wid $setw $seth $margin $marginh $wfac $position
+                        set cpos $position
+                    end
+                end
+                # if moved
+            end # if not focused
+        end
+        #  else if evading mode
+        DRAG_WINDOW_waitForEvent $evadeEnabled
+        echo $status >/dev/null
+    end
+    # while true
+
+end
+
+function DRAG_WINDOW_setCorner --description "INTERNAL: set the window to a specific corner"
+    set wid $argv[1]
+    set setw $argv[2]
+    set seth $argv[3]
+    set margin $argv[4]
+    set marginh $argv[5]
+    set wfac $argv[6]
+    set position $argv[7]
+
+    # get window name for printing
+    set wname (xprop -id $wid | egrep "_NET_WM_NAME" | sed -r 's/^_NET_WM_NAME.[^)]*.\s*=\s*"([^"]*)"/\1/g')
     # screen-size
     set w (i3-msg -t get_workspaces | jq '.[] | select(.focused==true).rect.width')
     set h (i3-msg -t get_workspaces | jq '.[] | select(.focused==true).rect.height')
@@ -144,8 +224,11 @@ function drag_window --description "drag the clicked window to all visited works
     set winHeight (echo $windata | jq '.rect.height')
     # set width if specified
     if [ "$setw" != false ]
-        echo -e "Adjusting width to \e[35m"(math "floor(floor((($w/100)*$setw)*$wfac)/$w*100)")"\e[0m%. of screen size"
-        i3-msg "[id=\"$wid\"] resize set width "(math "floor((($w/100)*$setw)*$wfac)") >/dev/null
+        set nw (math "floor($wfac*($setw * $w)/100)")
+        if [ "$nw" != "$winWidth" ]
+            echo -e "Adjusting width from \e[35m$winWidth\e[0mpx to \e[35m"$nw"\e[0mpx"
+            i3-msg "[id=\"$wid\"] resize set width "$nw >/dev/null
+        end
         # window-data (refresh after resize)
         set windata (i3-msg -t get_tree | jq '.. | select(.window=='$wid')? | .')
         # window current dimensions
@@ -156,8 +239,11 @@ function drag_window --description "drag the clicked window to all visited works
     end
     # set height if specified
     if [ "$seth" != false ]
-        echo -e "Adjusting height to ratio \e[35m$setw\e[0m/\e[35m$seth\e[0m."
-        i3-msg "[id=\"$wid\"] resize set height "(math "floor($winWidth*($seth/$setw))") >/dev/null
+        set nh (math "floor($winWidth*($seth/$setw))")
+        if [ "$nh" != "$winHeight" ]
+            echo -e "Adjusting height from \e[35m$winHeight\e[0mpx to \e[35m$nh\e[0mpx"
+            i3-msg "[id=\"$wid\"] resize set height "$nh >/dev/null
+        end
         # window-data (refresh after resize)
         set windata (i3-msg -t get_tree | jq '.. | select(.window=='$wid')? | .')
         # window current dimensions
@@ -193,54 +279,67 @@ function drag_window --description "drag the clicked window to all visited works
             set y (math "($h-$winHeight-$margin)")
     end
 
-    i3-msg "[id=\"33554437\"] move absolute position $x $y" >/dev/null
+    echo -e "Moving window \e[93m$wname\e[0m to position \e[94m$position\e[0m."
+    i3-msg "[id=\"$wid\"] move absolute position $x $y" >/dev/null
+end
+function DRAG_WINDOW_getCorner --description "INTERNAL: get the current corner the mouse is in"
+    # screen-size
+    set w (i3-msg -t get_workspaces | jq '.[] | select(.focused==true).rect.width')
+    set h (i3-msg -t get_workspaces | jq '.[] | select(.focused==true).rect.height')
 
-    ###
-    ### Main window dragging script
-    ###
-    ##  updates whenever a window is changed
-    ##  -> to use less resources, and detect workspace-changes upon that
-    ##
-    set pws UNSET
-    while true
-        # main function loop
-        # monitor for switching workspaces
-        set cws (i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
-        if [ "$cws" != "$pws" ]
-            # move window if workspace changed and update last move target
-            i3-msg '[id="'$wid'"] move to workspace '$cws >/dev/null
-            if [ "$pws" != UNSET ]
-                echo -e "Moving \e[95m$wname\e[0m from WS \e[33m$pws\e[0m to \e[92m$cws\e[0m"
-            end
-            set pws $cws
-        end
-        # if $cws != $pws
-        # wait for a title change or timeout (wichever earlier) to trigger a refresh
-        timeout 1m xtitle -s | read >/dev/null
-        echo $status >/dev/null
+    set -l mp (xdotool getmouselocation --shell)
+    set -l x (echo $mp[1] | egrep -o "[0-9]*")
+    set -l y (echo $mp[2] | egrep -o "[0-9]*")
+
+    set -l top true
+    if [ (math "floor($y/$h*2)") -gt 0 ]
+        set top false
+        echo -n b
+    else
+        echo -n t
     end
-    # while true
+    set -l right false
+    if [ (math "floor($x/$w*2)") -gt 0 ]
+        set right true
+        echo -n r
+    else
+        echo -n l
+    end
 
 end
-# function
-function DRAG_WINDOW_checkSystem --description "check system for required binaries and provide install-instructions in case of missing software"
-    if not which xtitle >/dev/null
-        echo "# xtitle script for showing the current window title"
-        echo "apt -y install libxcb-ewmh-dev"
-        echo "git clone --recursive --depth 1 https://github.com/baskerville/xtitle"
-        echo "pushd xtitle"
-        echo make
-        echo "sudo make install"
-        echo popd
-        echo "rm -rf xtitle"
+
+function DRAG_WINDOW_checkRunning --description "INTERNAL: check if the background listeners for update-events are running"
+    set -l j (jobs)
+    set -l jobsXtitle (echo $j | egrep "xtitle" | wc -l)
+    if [ $jobsXtitle -gt 0 ]
+        echo xtitle
     end
-    if not which xdotool >/dev/null
-        echo "# xdotool for identifying windows"
-        echo "sudo apt install -y xdotool"
+    set -l mousemove (echo $j | egrep "getmouselocation" | wc -l)
+    if [ $mousemove -gt 0 ]
+        echo getmouselocation
     end
-    if not which jq >/dev/null
-        echo "# jq for parsing and going through json data"
-        echo "sudo apt install -y jq"
+end
+
+function DRAG_WINDOW_waitForEvent --description "INTERNAL: waits for an event occur that would justify an update"
+    set -l c (DRAG_WINDOW_checkRunning)
+
+
+    if ! $argv[1]
+        timeout 1m xtitle -s | read >/dev/null
     end
-    echo -e "# autogenerated install script done!"
+
+    # wait for a title change or timeout (wichever earlier) to trigger a refresh
+    if [ (echo $c | egrep -o "xtitle" | wc -l) -lt 1 ]
+        fish -c "timeout 1m xtitle -s | read >/dev/null" &
+    end
+    if [ (echo $c | egrep -o "getmouselocation" | wc -l) -lt 1 ]
+        fish -c 'set -l mp (xdotool getmouselocation --shell);xdotool mousemove --sync (echo $mp[1] | egrep -o "[0-9]*") (echo $mp[2] | egrep -o "[0-9]*")' &
+    end
+    while true
+        set -l r (DRAG_WINDOW_checkRunning | wc -l)
+        if [ $r -lt 2 ]
+            return
+        end
+        sleep 0.1
+    end
 end
